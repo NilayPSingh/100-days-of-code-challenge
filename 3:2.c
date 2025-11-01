ceb2e20d19a06159ae9de46897f1c53a127eb2d1
NilayPSingh <nilay.pratap.5596@gmail.com>
2025-11-02 00:10:09 +0530

    swap two numbers using third variable

---
+++ 3:2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    printf("\nBefore swapping:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("\nAfter swapping:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    return 0;
+}
